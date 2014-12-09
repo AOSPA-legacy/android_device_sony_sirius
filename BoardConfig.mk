517ff2b31c4a6aa558d015f467ae674839906ff5
TheMrcool212 <themrcool212.android@gmail.com>
2014-12-09 21:20:57 +0800

    Remove unused config

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,9 +30,6 @@ BOARD_HARDWARE_CLASS += device/sony/sirius/cmhw
 # Kernel properties
 TARGET_KERNEL_CONFIG := cm_shinano_sirius_defconfig
 
-# External apps on SD
-TARGET_EXTERNAL_APPS = sdcard1
-
 # Partition information
 BOARD_VOLD_MAX_PARTITIONS := 25
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 12656242688 # 12656259072 - 16384
